109e19ca1f73e070650d9a17fae5e4435b6ae8a6
pisaacson <patrick.isaacson@gmail.com>
2020-10-17 15:37:13 -0500

    Revert "Update ipmi.js"
    This reverts commit 9e4e6fa4ced04bb7b19fb2eae5b4dc3ed15b1eee.

--- ipmi.js
+++ ipmi.js
@@ -9,73 +9,6 @@ module.exports = (homebridge) => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  var FakeGatoHistoryService = require('fakegato-history')(homebridge);
-
-	/* Try to map Elgato custom vars */
-	CurrentVoltageReading = function () {
-		Characteristic.call(this, 'Volt', 'E863F10A-079E-48FF-8F27-9C2605A29F52');
-		this.setProps({
-			format: Characteristic.Formats.FLOAT,
-			unit: "Volts",
-			maxValue: 1000,
-			minValue: 0,
-			minStep: 0.1,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	CurrentPowerConsumption = function () {
-		Characteristic.call(this, 'Consumption', 'E863F10D-079E-48FF-8F27-9C2605A29F52');
-		this.setProps({
-			format: Characteristic.Formats.UINT16,
-			unit: "Watts",
-			maxValue: 1000,
-			minValue: 0,
-			minStep: 1,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	CurrentPowerConsumption.UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
-	inherits(CurrentPowerConsumption, Characteristic);
-
-	TotalConsumption = function () {
-		Characteristic.call(this, 'Energy', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
-		this.setProps({
-			format: Characteristic.Formats.FLOAT,
-			unit: "kWh",
-			maxValue: 100000000000,
-			minValue: 0,
-			minStep: 0.001,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	TotalConsumption.UUID = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
-	inherits(TotalConsumption, Characteristic);
-
-	ResetTotal = function () {
-		Characteristic.call(this, 'Reset', 'E863F112-079E-48FF-8F27-9C2605A29F52');
-		this.setProps({
-			format: Characteristic.Formats.UINT32,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY, Characteristic.Perms.WRITE]
-		});
-		this.value = this.getDefaultValue();
-	};
-	ResetTotal.UUID = 'E863F112-079E-48FF-8F27-9C2605A29F52';
-	inherits(ResetTotal, Characteristic);
-
-	PowerMeterService = function (displayName, subtype) {
-		Service.call(this, displayName, '00000001-0000-1777-8000-775D67EC4377', subtype);
-		this.addCharacteristic(CurrentPowerConsumption);
-		this.addCharacteristic(TotalConsumption);
-		this.addCharacteristic(ResetTotal);
-	};
-	inherits(PowerMeterService, Service);
-
-	FakeGatoHistoryService = FakeGatoHistoryService;
-	inherits(FakeGatoHistoryService, Service);
-
   homebridge.registerAccessory('homebridge-ipmi', 'IPMI', IPMIPlugin);
 };
 
@@ -102,7 +35,6 @@ class IPMIPlugin
                         'FAN 4': 'Fan 4',
                         'FAN A': 'Fan A'
                 };
-    this.power = config.power
     this.identify = config.identify !== undefined ? config.identify : "Blink";
 
     this.server = new IPMI(this.hostname, this.username, this.password);
@@ -118,7 +50,7 @@ class IPMIPlugin
         .getCharacteristic(Characteristic.On)
         .on('get', this.getIdentify.bind(this))
         .on('set', this.setIdentify.bind(this));
-      this.sensors.push(switchSensor);
+      this.sensors.push(switchSensor);	     
     }
 
     Object.keys(this.temperatureSensors).forEach((ipmiName) => {
